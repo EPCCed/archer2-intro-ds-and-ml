c7754f43bf967673059de3d55fc76330023920f4
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2025-08-12 00:40:07 +0100

    Updating script to latest project

--- scripts/run-interactive.sh
+++ scripts/run-interactive.sh
@@ -1,3 +1,3 @@
-srun --nodes=1 --exclusive --time=00:20:00 --account=ta160 \
+srun --nodes=1 --exclusive --time=00:20:00 --account=ta209 \
      --partition=standard --qos=short --reservation=shortqos \
      --pty /bin/bash
